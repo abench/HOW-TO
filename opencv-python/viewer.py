8d67fa6bb6ebbbdcc07d81f6123e6af88e3b5529
abench <abench@users.noreply.github.com>
2017-09-05 15:12:12 +0300

    First commit of viewer.py script
    Viewer script was created
    Some refactoring in video_writing script was made

--- opencv-python/viewer.py
+++ opencv-python/viewer.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep  5 12:38:45 2017
+Created on Tue Sep  5 15:06:50 2017
 
 @author: abench
 """
@@ -10,20 +10,14 @@ import cv2
 camera_num=0
 camera=cv2.VideoCapture(camera_num)
 
-
-fourcc=cv2.VideoWriter_fourcc(*'XVID')
-out=cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
-
 while(camera.isOpened()):
     ret,frame=camera.read()
     if ret==True:
         frame=cv2.flip(frame,1)
-        out.write(frame)
         cv2.imshow('frame',frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         break
 camera.release()
-out.release()
 cv2.destroyAllWindows()
